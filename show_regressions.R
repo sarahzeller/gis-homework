ac342357dec26e223b8b7dba0b36f261bf7cacf7
szeller42 <s.zeller@posteo.net>
2023-05-30 12:04:30 +0200

    🎨 touch up layout

--- show_regressions.R
+++ show_regressions.R
@@ -4,5 +4,11 @@ show_regressions <- function(regression_list) {
                stars = TRUE,
                coef_rename = TRUE,
                coef_omit = "Intercept",
-               gof_omit = "IC|Adj|RMSE")
+               gof_omit = "IC|Adj|RMSE",
+               align = paste(c(
+                 "l", 
+                 rep("d", length(regression_list))
+                 ),
+                              collapse = "")) |> 
+    landscape()
 }
